9e0860db77b497d5b33345430b958594579bc1c6
Nicolas. Z <ni.zunigao@duocuc.cl>
2024-11-29 21:47:48 -0300

    vista viaje

--- src/app/pages/viaje/viaje.page.spec.ts
+++ src/app/pages/viaje/viaje.page.spec.ts
@@ -1,11 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ViajePage } from './viaje.page';
+import { ActivatedRoute } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('ViajePage', () => {
   let component: ViajePage;
   let fixture: ComponentFixture<ViajePage>;
-
+  
+  const mock = {
+    snapshot:{
+      params: { destino : 'asd123'}
+    }
+  }
+ 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers:[provideHttpClient()]
+    })
+    TestBed.configureTestingModule({
+      providers:[{provide: ActivatedRoute,useValue:mock}]
+    })
     fixture = TestBed.createComponent(ViajePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/pages/viaje/viaje.page.ts
+++ src/app/pages/viaje/viaje.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { StorageService } from 'src/app/services/storage.service';
+import { ViajeService } from 'src/app/services/viaje.service';
 
 @Component({
   selector: 'app-viaje',
@@ -7,9 +10,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViajePage implements OnInit {
 
-  constructor() { }
+
+  viajes:any[]=[];
+  
+
+  constructor(private activateroute: ActivatedRoute,
+              private viajeService:ViajeService,
+              private storage:StorageService,
+              private router:Router
+
+   ) {}
 
   ngOnInit() {
+    this.cargarViajes();
   }
 
+  detalles(viaje: any) {
+    viaje.showDetails = !viaje.showDetails;
+  }
+
+  async cargarViajes() {
+    try {
+      
+      let dataStorage = await this.storage.obtenerStorage();
+  
+      if (dataStorage && dataStorage.length > 0) {
+        
+        const req = await this.viajeService.obtenerViaje(dataStorage[0].token);
+  
+        if (req && req.data) {
+          this.viajes = req.data;
+          console.log("DATA VIAJES OBTENIDOS", this.viajes);
+        } else {
+          console.error("No se encontraron datos de viajes en la respuesta.");
+        }
+      } else {
+        console.error("No se encontró información en el almacenamiento.");
+      }
+    } catch (error) {
+      console.error("Error al cargar los viajes:", error);
+    }
+  }
+  
+
+  agregarViaje(){
+    this.router.navigateByUrl("");
+  }
+  
 }
