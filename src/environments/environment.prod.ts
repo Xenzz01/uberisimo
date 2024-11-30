69515c9475258f4796ee1959b89894cdf4123921
Nicolas. Z <ni.zunigao@duocuc.cl>
2024-11-29 22:12:33 -0300

    capacitor android

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBHsGMKJ5Ow7mL7qanZxqz-cgBLSXONV5g",
+    authDomain: "uber-app-e936a.firebaseapp.com",
+    projectId: "uber-app-e936a",
+    storageBucket: "uber-app-e936a.appspot.com",
+    messagingSenderId: "616558101807",
+    appId: "1:616558101807:web:9fb0290b53d45a0d078dda"
+  },
+  apiUrl: "https://uber-nodejs-server-git-d61f89-guillermovillacuratorres-projects.vercel.app/api/"
 };
