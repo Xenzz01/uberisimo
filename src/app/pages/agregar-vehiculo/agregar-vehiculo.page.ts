53968bc11cab8da8c6a60dc83015a001e3a3a735
Nicolas. Z <ni.zunigao@duocuc.cl>
2024-11-29 22:03:42 -0300

    vista agregar-vehiculo

--- src/app/pages/agregar-vehiculo/agregar-vehiculo.page.ts
+++ src/app/pages/agregar-vehiculo/agregar-vehiculo.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraResultType } from '@capacitor/camera';
 
 @Component({
   selector: 'app-agregar-vehiculo',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./agregar-vehiculo.page.scss'],
 })
 export class AgregarVehiculoPage implements OnInit {
-
+  imagen:any;
+  marca: string ="";
+  modelo: string ="";
+  
   constructor() { }
 
   ngOnInit() {
   }
+
+
+
+  async takePhoto(){
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri
+    });
+    if(image.webPath){
+      const response = await fetch(image.webPath);
+      const blob = await response.blob();
+
+      this.imagen = {
+        fname: 'foto' + image.format,
+         src:image.webPath,
+         file: blob
+      }
+    }
+    var imageUrl = image.webPath;
+    this.imagen.src = imageUrl;
+}
+
 
 }
