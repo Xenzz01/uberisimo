bc34822a4885f046d8ed8f5f209bbd58899d287e
Nicolas. Z <ni.zunigao@duocuc.cl>
2024-12-05 17:37:09 -0300

    modificaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+// Importa HttpClientModule
+import { HttpClientModule } from '@angular/common/http';
+
+// Importaciones de AngularFire
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule, 
+    AngularFireModule.initializeApp(environment.firebaseConfig), 
+    AngularFireAuthModule, 
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
-//comentario
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,19 +1,23 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
   production: false,
    firebaseConfig : {
-    apiKey: "AIzaSyBHsGMKJ5Ow7mL7qanZxqz-cgBLSXONV5g",
-    authDomain: "uber-app-e936a.firebaseapp.com",
-    projectId: "uber-app-e936a",
-    storageBucket: "uber-app-e936a.appspot.com",
-    messagingSenderId: "616558101807",
-    appId: "1:616558101807:web:9fb0290b53d45a0d078dda"
+    apiKey: "AIzaSyBsTka_ypJGbZbwLMm36rbwsLs1S1GHvQ4",
+    authDomain: "uberisimo-6368f.firebaseapp.com",
+    projectId: "uberisimo-6368f",
+    storageBucket: "uberisimo-6368f.firebasestorage.app",
+    messagingSenderId: "1088000895856",
+    appId: "1:1088000895856:web:31ecfc587fc9c82535fdb5",
+    measurementId: "G-T7H8E1SYDQ"
   },
   apiUrl: "https://uber-nodejs-server-git-d61f89-guillermovillacuratorres-projects.vercel.app/api/"
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
