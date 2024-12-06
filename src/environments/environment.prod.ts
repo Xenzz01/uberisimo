b57f61b9042f886921112da7eb74f9e5452f5afa
nz <ni.zunigao@duocuc.cl>
2024-12-06 18:08:08 -0300

    enviro

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: "AIzaSyBHsGMKJ5Ow7mL7qanZxqz-cgBLSXONV5g",
-    authDomain: "uber-app-e936a.firebaseapp.com",
-    projectId: "uber-app-e936a",
-    storageBucket: "uber-app-e936a.appspot.com",
-    messagingSenderId: "616558101807",
-    appId: "1:616558101807:web:9fb0290b53d45a0d078dda"
+    apiKey: "AIzaSyBsTka_ypJGbZbwLMm36rbwsLs1S1GHvQ4",
+    authDomain: "uberisimo-6368f.firebaseapp.com",
+    projectId: "uberisimo-6368f",
+    storageBucket: "uberisimo-6368f.firebasestorage.app",
+    messagingSenderId: "1088000895856",
+    appId: "1:1088000895856:web:31ecfc587fc9c82535fdb5",
+    measurementId: "G-T7H8E1SYDQ"
   },
   apiUrl: "https://uber-nodejs-server-git-d61f89-guillermovillacuratorres-projects.vercel.app/api/"
 };
